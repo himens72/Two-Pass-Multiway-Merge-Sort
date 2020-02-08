d0dacf8e55e73f6ff525b1b284af8751719c673f
Himen Sidhpura <himens72@gmail.com>
2020-02-08 17:59:08 -0500

    PhaseOne.java and PhaseTwo.java
    Signed-off-by: Himen Sidhpura <himens72@gmail.com>

--- src/com/two/pass/multiway/mergesort/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.two.pass.multiway.mergesort;
-
-public class Constants {
-
-	public static final int TUPLE_SIZE = 100;
-	public static final int BLOCK_SIZE = (4096 / TUPLE_SIZE) * TUPLE_SIZE;
-	public static final int MAIN_MEMORY_SIZE = (((int)(Runtime.getRuntime().totalMemory() * 0.5) / BLOCK_SIZE) * BLOCK_SIZE);
-}
